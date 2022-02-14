a4ec2e7113d11a9bfff0e768db3e3d9ee9fae21e
Jakub Gorski <supersaiyan.iv@gmail.com>
2022-02-14 16:08:36 +0100

    aoc: day10: main.cpp prints puzzle input
    Activity: Improvement

--- day10/main.cpp
+++ day10/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!";
+    std::ifstream in("input.txt");
+    std::string input((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+    std::cout << input;
+
     return 0;
 }
